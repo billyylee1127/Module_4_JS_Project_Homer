db09760770aa70602bfa64fea24bf88213d65ae7
Billy Lee <billylee@billys-air.mynetworksettings.com>
2026-01-14 17:21:59 -0600

    input button

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 async function fetchMovies() {
-    const data = await fetch("https://www.omdbapi.com/?apikey=95660dd5&s=fast")
-    const response = await data.json()
-    console.log(
-    response.map(
-        (data) => `<div class="movie-card">
-        <div class="movie-card__container">
-            <img src=".movie-card.json">
-            <h3>Movie Title</h3>
-            <p><b>Type:</b>movie</p>
-            <p><b>Year:</b>2025</p>
-        </div>
-    </div>`
-        )
-    );
+    const res = await fetch(`https://www.omdbapi.com/?apikey=95660dd5&s=fast`)
+    const data = await res.json()
+    const searchResultsEl = document.querySelector(`.search-results`)
+    console.log(data) 
+
+    searchResultsEl.innerHTML = data.Search.map((movie) => searchHTML(movie)).join("")        
 }
 
 fetchMovies()
+
+function searchHTML(movie) {
+    return `<div class="movie-card">
+            <div class="movie-card__container">
+                <img src="${movie.Poster}">
+                <h3>${movie.Title}</h3>
+                <p><b>Type:</b>${movie.Type}</p>
+                <p><b>Year:</b>${movie.Year}</p>
+            </div>
+        </div>` 
+}
